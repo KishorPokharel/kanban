fa5b9eb85788c6e998b8cc25806025e3b25e834b
KishorPokharel <kishorpokharel7@gmail.com>
2023-03-15 21:20:35 +0545

    remove indent in json output

--- response.go
+++ response.go
@@ -15,7 +15,9 @@ func (app *application) errorResponse(w http.ResponseWriter, status int, message
 		"success": false,
 		"message": message,
 	}
-	if err := json.NewEncoder(b).Encode(&out); err != nil {
+	enc := json.NewEncoder(b)
+	enc.SetIndent("", "")
+	if err := enc.Encode(&out); err != nil {
 		app.logger.Println(err)
 		return
 	}
@@ -26,7 +28,9 @@ func (app *application) jsonResponse(w http.ResponseWriter, status int, out any)
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(status)
 	b := &bytes.Buffer{}
-	if err := json.NewEncoder(b).Encode(&out); err != nil {
+	enc := json.NewEncoder(b)
+	enc.SetIndent("", "")
+	if err := enc.Encode(&out); err != nil {
 		app.errorResponse(w, http.StatusInternalServerError, "Something went wrong", err)
 		return
 	}
